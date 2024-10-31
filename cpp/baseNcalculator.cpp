ac35464d60c83d84c01da19d3e5df34ef8f49ff9
Nahin Munkar <92095985+NahinM@users.noreply.github.com>
2024-10-31 19:55:47 +0600

    Update baseNcalculator.cpp

--- cpp/baseNcalculator.cpp
+++ cpp/baseNcalculator.cpp
@@ -72,11 +72,15 @@ string Number::operator-(Number &num2){
 }
 
 int main(){
-    freopen("input.txt","r",stdin);
+    // freopen("input.txt","r",stdin);
     int base;
     string s1,s2;
-    cin  >> base >> s1 >> s2;
+    // cin  >> base >> s1 >> s2;
+    base = 16;
+    s1 = AA;
+    s2 = 11;
     Number n1 = Number(s1,base);
     Number n2 = Number(s2,base);
     cout << n1 - n2;br;
+    cout << n1 + n2;br;
 }
